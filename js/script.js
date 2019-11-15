3d207845fda9b2ef94bf31b5ad712a37e75e1452
Armen <armen9715@gmail.com>
2019-11-16 03:34:43 +0400

    Add History

--- js/script.js
+++ js/script.js
@@ -1,73 +1,51 @@
 var screen = document.getElementById("screen");
 var calculator = document.getElementById("calculator");
 var operators = document.getElementsByClassName("operator");
-
-
 screen.onkeyup = keyUp;
-var q = " ";
+var q=" "
+var a=" "
+var z = "";
 
 function keyUp(e) {
     var x = e.key;
-    var regexNumbers = /^[0-9]+$/;
-    var regexSigns = new RegExp(/[\.\+\-\/\*]+$/g);
+    console.log(x)
 
-    if (x.match(regexSigns) || x.match(regexNumbers)) {
-         q=screen.value;
+    var regexNumbers = /[0-9\+\*\-\%\/]+/gm;
+    if (x.match(regexNumbers) || x == "Backspace") {
+        z = screen.value;
 
+    } else if (x == "Enter" || x == "=") {
+        screen.value = z;
+        result()
     } else {
-        screen.value = q;
+        screen.value = z;
         error();
     }
 }
+
 function input(i) {
     screen.value = screen.value + i;
-
-
-    //Animate js
-    calculator.style.width = "325px";
-    setTimeout(function () {
-        for (var y = 0; y < operators.length; y++) {
-            operators[y].style.display = "block";
-
-
-        }
-
-    }, 700);
-
-    setTimeout(function () {
-        for (var y = 0; y < operators.length; y++) {
-            operators[y].style.opacity = "1";
-            operators[y].style.pointerEvents = "auto";
-
-
-        }
-
-    }, 800);
-
-
+    z = screen.value;
 }
 
 function result() {
-    if (eval(screen.value) != undefined) {
-        screen.value = eval(screen.value);
+    if (eval(screen.value) == undefined) {
+        q = " ";
+        screen.value = "Error";
 
-    } else {
-        screen.value = '';
+    } else if(isNaN(eval(screen.value))){
+        screen.value="Error"
+    }
+
+    else {
+        history(screen.value, eval(screen.value))
+        screen.value = eval(screen.value);
     }
 }
 
 function reset() {
-
+    z = "";
     screen.value = "";
-
-    //Animate js
-    for (var y = 0; y < operators.length; y++) {
-        operators[y].style.opacity = "0.4";
-        operators[y].style.pointerEvents = "none";
-
-    }
-
-
 }
 
 function error() {
@@ -75,7 +53,54 @@ function error() {
 
     screen.style.backgroundColor = "#9e4249"
     setTimeout(function () {
-        screen.style.backgroundColor = "#d3545d";
+        screen.style.backgroundColor = "#00000096";
     }, 300);
 }
+
+// function animation() {
+//     if (screen.value.length > 0) {
+//         calculator.style.width = "325px";
+//         setTimeout(function () {
+//             for (y = 0; y < operators.length; y++) {
+//                 operators[y].style.display = "block";
+//
+//
+//             }
+//
+//         }, 700);
+//
+//         setTimeout(function () {
+//             for (y = 0; y < operators.length; y++) {
+//                 operators[y].style.opacity = "1";
+//                 operators[y].style.pointerEvents = "auto";
+//
+//
+//             }
+//
+//         }, 800);
+//     } else if (screen.value.length == 0){
+//         for (y = 0; y < operators.length; y++) {
+//             operators[y].style.opacity = "0.4";
+//             operators[y].style.pointerEvents = "none";
+//
+//         }
+//
+//
+//     }
+//
+//
+// }
+
+function history(a,b) {
+    var para = document.createElement("P");
+    para.innerHTML = a + "="+b;
+    document.getElementById("history").appendChild(para);
+
+}
+
+function backSpace() {
+    screen.value= screen.value.substring(0, screen.value.length - 1);
+
+}
+
 
